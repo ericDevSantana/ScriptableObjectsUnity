c13173f066bb4643d36a46225232a5550e7a9476
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-03-11 14:21:55 -0700

    First Commit

---
+++ Assets/Scriptable Objects - Players/PlayerBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public ScriptablePlayer player;
+    public Text description;
+
+    private void OnMouseDown()
+    {
+        description.text = "Player's Name: " + player.playerName +
+            "\nPlayer's description: " + player.playerDescription +
+            "\nPlayer's health: " + player.playerBaseHealth +
+            "\nPlayer's base attack: " + player.playerBaseAttack +
+            "   \nPlayer's base defense: " + player.playerBaseDefense;
+    }
+}
---
+++ Assets/Scriptable Objects - Players/ScriptablePlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player", menuName = "NewPlayer", order = 1)]
+public class ScriptablePlayer : ScriptableObject
+{
+    public string playerName;
+    public string playerDescription;
+
+    public float playerBaseHealth;
+    public float playerBaseAttack;
+    public float playerBaseDefense;
+           
+}
